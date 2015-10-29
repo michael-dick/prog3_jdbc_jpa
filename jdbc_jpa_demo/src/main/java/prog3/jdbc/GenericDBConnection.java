92de7b2ef63d54fbc2233ec84ea25263fa752dec
Michael Dick <michaeldick94@web.de>
2015-10-29 17:00:36 +0100

    added comments

--- jdbc_jpa_demo/src/main/java/prog3/jdbc/GenericDBConnection.java
+++ jdbc_jpa_demo/src/main/java/prog3/jdbc/GenericDBConnection.java
@@ -4,19 +4,23 @@ import java.sql.*;
 
 /**
  * Created by MichaelDick on 28/10/15.
+ * We use this abstacr class for providing the methods connect(String dbName) and disconnect()
+ * the other methods are kept abstract so the logic still has to be implemented
  */
 
-//bereitgestellt
 public abstract class GenericDBConnection<KEY , TYPE> {
 
+    //java.sql.Connection etabished the connection to our target database
     public Connection connection;
 
+    //java.sql.Statement is able to perform SQL-Queries on out database
     public Statement mStatement;
 
+    //java.sql.PreparedStatement lets us set parameters(?) in our query to keep the query dynamic
     public PreparedStatement mPreparedStatement;
 
     public void connect(String dbName){
-        //load jdbc driver
+        //load jdbc driver --> unnecessary beacause we use maven
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             System.out.println("driver loaded");
@@ -27,6 +31,8 @@ public abstract class GenericDBConnection<KEY , TYPE> {
 
         //Connection
         try {
+            //Connect to database --> name specified
+            //username is root, pw is empty --> NOT RECOMMENDED
             connection = DriverManager.getConnection
                     ("jdbc:mysql://localhost/" + dbName, "root", "");
 
